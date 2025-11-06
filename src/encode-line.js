2b73300605ad117018d99533728473979af2b461
ilyasul <i.sul95@gmail.com>
2025-11-06 15:40:27 +0300

    feat: done task encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  if (!str) return '';
+  let count = 0;
+  let name = '';
+  const result = [];
+  for (const item of [...str]) {
+    if (name !== item && name !== '') {
+      count === 1 ?
+        result.push(`${name}`) :
+        result.push(`${count}${name}`);
+      count = 0;
+    }
+    console.log(item);
+    name = item;
+    count += 1;
+  }
+  count === 1 ?
+    result.push(`${name}`) :
+    result.push(`${count}${name}`);
+  console.log(result);
+  return result.join('')
 }
 
 module.exports = {
